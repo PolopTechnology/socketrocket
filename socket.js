d923ad202005d629722cbec65586b3e8cef42e22
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-08-05 21:21:31 -0400

    Add files via upload

--- socket.js
+++ socket.js
@@ -4,6 +4,12 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var PouchDB = require('PouchDB')
+var db = new PouchDB('socketdb');
+let areYouConnected = false;
+let numberOfMessages = 0;
+let n = numberOfMessages;
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -11,17 +17,67 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  //getfromdb
+  if(areYouConnected === false) {
+    db.get('101', function(err,docu){
+      if(err) {
+        console.log('Diego dice que tienes un error. Arreglalo AHORA!');
+        console.log(err);
+      } else {
+        var keys = [];
+        var values = [];
+        for(var value in docu){
+          values.push(docu[value]);
+          console.log(docu[value]);
+        }
+        for(var key in docu){
+          keys.push(key);
+          console.log('------------')
+          console.log(key);
+        }
+        for(let c = 0; c < keys.length; c++){
+          if(keys[c] != '_id' && keys[c] != '_rev' && keys[c] != 'numberOfMessages'){
+            io.emit('chat message', values[c]);
+          }
+        }
+        console.log(docu);
+      }
+    })
+    areYouConnected = true;
+  }
+  //getfromdb
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+server.listen(3000, "192.168.0.7", () => {
   console.log('listening on *:3000');
 });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
+      //db
+      db.get('101', function(err,docu){
+        if(err){
+          console.log('Diego dice que tienes un error. Arreglalo AHORA!');
+          console.log(err);
+        } else {
+          docu['message' + n.toString()] = msg;
+          console.log(n);
+          console.log(areYouConnected);
+          n += 1;
+          return db.put(docu, function(error,res){
+            if(error) {
+              console.log('Diego dice que tienes un error. Arreglalo AHORA!');
+              console.log(error);
+            } else {
+              console.log('msg saved!');
+            }
+          })
+        }
+      })
+      //db
       console.log('message: ' + msg);
     });
   });
